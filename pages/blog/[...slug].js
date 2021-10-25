ac16d9615d560df122b5b2c659e4be49c4d5f0ff
goleedev <golee.dev@gmail.com>
2021-10-25 20:12:58 +0900

    12.8 Update Post

--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -56,7 +56,7 @@ export default function Blog({ post, authorDetails, prev, next }) {
       ) : (
         <div className="mt-24 text-center">
           <PageTitle>
-            Under Construction{' '}
+            공사 중{' '}
             <span role="img" aria-label="roadwork sign">
               🚧
             </span>
