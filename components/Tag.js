021fe25854f3f1d846fc2154a121973a47628d08
goleedev <golee.dev@gmail.com>
2021-10-26 16:11:40 +0900

    17.0 Update Tag

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+      <a className="mr-3 text-sm bg-blue-500 px-1 py-0.5 rounded-sm font-medium uppercase text-white transition-all duration-100 ease-in-out hover:bg-blue-600 dark:text-blue-600 dark:bg-blue-200 dark:hover:bg-blue-300">
         {text.split(' ').join('-')}
       </a>
     </Link>
