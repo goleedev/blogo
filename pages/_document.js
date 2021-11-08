181f79c890808f9ae0ab5ae6a31c53ec9b4aaaa4
goleedev <golee.dev@gmail.com>
2021-11-08 22:53:50 +0900

    29.6 Add Post 1108

--- pages/_document.js
+++ pages/_document.js
@@ -36,7 +36,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/katex@0.13.11/dist/katex.min.css"
             integrity="sha384-Um5gpz1odJg5Z4HAmzPtgZKdTBHZdw8S29IecapCSB31ligYPhHQZMIlWLYQGVoc"
-            crossOrigin="anonymous"
+            crossOrigin="true"
           />
         </Head>
         <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
